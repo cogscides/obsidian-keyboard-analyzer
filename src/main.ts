f10dc2806fe830c56d2a351d8b1dc85ce80b1624
cσgscídєs <50235526+cogscides@users.noreply.github.com>
2021-11-12 01:40:33 +0200

    commit start

--- src/main.ts
+++ src/main.ts
@@ -1,117 +1,123 @@
-import { addIcon, Notice, Plugin, WorkspaceLeaf } from 'obsidian'
-import { openView, wait } from 'obsidian-community-lib'
-import AnalysisView from 'src/AnalysisView'
 import {
-  DEFAULT_SETTINGS,
-  iconSVG,
-  VIEW_TYPE_GRAPH_ANALYSIS,
-} from 'src/constants'
-import { clusteringCoefficient, findTrianglesForNode } from 'src/GeneralGraphFn'
-import type { GraphAnalysisSettings } from 'src/Interfaces'
-import MyGraph from 'src/MyGraph'
-import { SampleSettingTab } from 'src/Settings'
-import { debug } from './Utility'
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from 'obsidian'
+import { openView, wait } from 'obsidian-community-lib'
 
-export default class GraphAnalysisPlugin extends Plugin {
-  settings: GraphAnalysisSettings
-  g: MyGraph
+// Remember to rename these classes and interfaces!
+
+interface KeyboardAnalizerSettings {
+  mySetting: string
+}
+
+const DEFAULT_SETTINGS: KeyboardAnalizerSettings = {
+  mySetting: 'default',
+}
+
+export default class KeyboardAnalizerPlugin extends Plugin {
+  settings: KeyboardAnalizerSettings
 
   async onload() {
-    console.log('loading graph analysis plugin')
-
+    console.log('loading keyboard analizer plugin')
     await this.loadSettings()
-    addIcon('GA-ICON', iconSVG)
 
+    // This creates an icon in the left ribbon.
+    // const ribbonIconEl = this.addRibbonIcon(
+    //   'dice',
+    //   'Sample Plugin',
+    //   (evt: MouseEvent) => {
+    //     // Called when the user clicks the icon.
+    //     new Notice('Hello World, Madafaka!')
+    //   }
+    // )
+    // Perform additional things with the ribbon
+    // ribbonIconEl.addClass('my-plugin-ribbon-class')
+
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarEl = this.addStatusBarItem()
+    statusBarEl.setText('Shortcuts')
+    statusBarEl.addClass('mod-clickable')
+
+    this.registerDomEvent(statusBarEl, 'click', async () => {
+      console.log('click')
+    })
+    // let checkResult =
+    //   this.app.workspace.getLeavesOfType(VIEW_TYPE_GRAPH_ANALYSIS)
+    //     .length === 0
+
+    //   if (checkResult) {
+    //     // Only perform work when checking is false
+    //     if (!checking) {
+    //       openView(this.app, VIEW_TYPE_GRAPH_ANALYSIS, AnalysisView)
+    //     }
+    //     return true
+    //   }
+    // }
+
+    //     await this.disconnectDiscord();
+    //   }
+    // });
+
+    // This adds a simple command that can be triggered anywhere
     this.addCommand({
-      id: 'show-graph-analysis-view',
-      name: 'Open Graph Analysis View',
+      id: 'open-sample-modal-simple',
+      name: 'Open sample modal (simple)',
+      callback: () => {
+        new SampleModal(this.app).open()
+      },
+    })
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: 'sample-editor-command',
+      name: 'Sample editor command',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection())
+        editor.replaceSelection('Sample Editor Command')
+      },
+    })
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: 'open-sample-modal-complex',
+      name: 'Open sample modal (complex)',
       checkCallback: (checking: boolean) => {
-        let checkResult =
-          this.app.workspace.getLeavesOfType(VIEW_TYPE_GRAPH_ANALYSIS)
-            .length === 0
-
-        if (checkResult) {
-          // Only perform work when checking is false
+        // Conditions to check
+        const markdownView =
+          this.app.workspace.getActiveViewOfType(MarkdownView)
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
           if (!checking) {
-            openView(this.app, VIEW_TYPE_GRAPH_ANALYSIS, AnalysisView)
+            new SampleModal(this.app).open()
           }
+
+          // This command will only show up in Command Palette when the check function returns true
           return true
         }
       },
     })
 
-    this.addCommand({
-      id: 'refresh-analysis-view',
-      name: 'Refresh Graph Analysis View',
-      callback: async () => {
-        await this.refreshGraph()
-        const openView = this.app.workspace.getLeavesOfType(
-          VIEW_TYPE_GRAPH_ANALYSIS
-        )[0].view as AnalysisView
-        openView.draw()
-      },
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new KeyboardAnalyzerSettingTab(this.app, this))
+
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      console.log('click', evt)
     })
 
-    this.addCommand({
-      id: 'cluster-coeff',
-      name: 'cluster-coeff',
-      callback: async () => {
-        const currNode = this.app.workspace.getActiveFile().basename
-        const triangles = findTrianglesForNode(this.g, currNode)
-        const coef = clusteringCoefficient(this.g, currNode)
-        console.log({ triangles, coef })
-      },
-    })
-
-    this.addSettingTab(new SampleSettingTab(this.app, this))
-
-    this.registerView(
-      VIEW_TYPE_GRAPH_ANALYSIS,
-      (leaf: WorkspaceLeaf) => new AnalysisView(leaf, this)
-    )
-
-    this.app.workspace.onLayoutReady(async () => {
-      const noFiles = this.app.vault.getMarkdownFiles().length
-      while (!this.resolvedLinksComplete(noFiles)) {
-        await wait(1000)
-      }
-
-      await this.refreshGraph()
-      await openView(this.app, VIEW_TYPE_GRAPH_ANALYSIS, AnalysisView)
-    })
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    // this.registerInterval(
+    //   window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
+    // )
   }
 
-  resolvedLinksComplete(noFiles: number) {
-    const { resolvedLinks } = this.app.metadataCache
-    return Object.keys(resolvedLinks).length === noFiles
-  }
-
-  async refreshGraph() {
-    try {
-      console.time('Initialise Graph')
-      this.g = new MyGraph(this.app, this.settings)
-      await this.g.initGraph()
-      // await this.g.initData()
-      debug(this.settings, { g: this.g })
-      console.timeEnd('Initialise Graph')
-      new Notice('Index Refreshed')
-    } catch (error) {
-      console.log(error)
-      new Notice(
-        'An error occured with Graph Analysis, please check the console.'
-      )
-    }
-  }
-
-  onunload() {
-    console.log('unloading graph analysis plugin')
-    this.app.workspace
-      .getLeavesOfType(VIEW_TYPE_GRAPH_ANALYSIS)
-      .forEach((leaf) => {
-        leaf.view.unload()
-        leaf.detach()
-      })
-  }
+  onunload() {}
 
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
@@ -119,5 +125,52 @@ export default class GraphAnalysisPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings)
+  }
+}
+
+class SampleModal extends Modal {
+  constructor(app: App) {
+    super(app)
+  }
+
+  onOpen() {
+    const { contentEl } = this
+    contentEl.setText('Woah!')
+  }
+
+  onClose() {
+    const { contentEl } = this
+    contentEl.empty()
+  }
+}
+
+class KeyboardAnalyzerSettingTab extends PluginSettingTab {
+  plugin: KeyboardAnalizerPlugin
+
+  constructor(app: App, plugin: KeyboardAnalizerPlugin) {
+    super(app, plugin)
+    this.plugin = plugin
+  }
+
+  display(): void {
+    const { containerEl } = this
+
+    containerEl.empty()
+
+    containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' })
+
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter your secret')
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            console.log('Secret: ' + value)
+            this.plugin.settings.mySetting = value
+            await this.plugin.saveSettings()
+          })
+      )
   }
 }
